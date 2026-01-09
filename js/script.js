5cfa63a5e2ed882490e8e661d3f31633343ea323
crisavilaz <avilazcris@gmail.com>
2026-01-09 17:14:59 -0300

    feat: add calculator input validation

--- js/script.js
+++ js/script.js
@@ -1,15 +1,6 @@
-// Calculadora recebe um numero firstNumber
+// Operadores válidos
 
-// recebe um operador curentOperator
-
-// recebe outro numero secondNumber
-
-// função validadora
-
-//soma os numeros "=" operate()
-function operate(firstNumber, secondNumber){
-    
-}
+const VALID_OPERATORS = Object.freeze(["+", "-", "/", "*"]);
 
 // funções básicas, soma, subtração, divisão e multiplicação.
 
@@ -28,3 +19,46 @@ function multiply(a, b){
 function divide(a, b){
         return a / b;
 }
+
+// função validadora
+const validOperators = Object.freeze(["+", "-", "/", "*"]);
+
+function isValid(firstNumber, operator, secondNumber){
+
+    if (firstNumber === null ||
+        firstNumber === undefined ||
+        firstNumber === "") return false;
+    
+    if (secondNumber === null ||
+        secondNumber === undefined ||
+        secondNumber === "") return false;
+
+    if (!validOperators.includes(operator)) return false;
+
+    const a = Number(firstNumber);
+    const b = Number(secondNumber);
+
+    if (Number.isNaN(a) || Number.isNaN(b)) return false;
+
+    if (operator === "/" && b === 0) return false;
+
+    return true;
+}
+
+
+//soma os numeros "=" operate()
+
+function operate(firstNumber, operator, secondNumber){
+    
+}
+
+
+// Calculadora recebe um numero firstNumber
+
+// recebe um operador curentOperator
+
+// recebe outro numero secondNumber
+
+
+
+
