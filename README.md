55887ef35551b14d1e45aa666268ce93ec9bc8aa
crisavilaz <avilazcris@gmail.com>
2026-01-09 15:47:28 -0300

    Create: basic arithmetic operations

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # odin-calculator
 Calculadora básica de opreações
+
+Projeto final do curso básico de CSS, HTML e JS do Odin Project.
---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Calculadora recebe um numero firstNumber
+
+// recebe um operador curentOperator
+
+// recebe outro numero secondNumber
+
+// função validadora
+
+//soma os numeros "=" operate()
+function operate(firstNumber, secondNumber){
+    
+}
+
+// funções básicas, soma, subtração, divisão e multiplicação.
+
+function add(a, b){
+        return a + b;
+}
+
+function substract(a, b){
+        return a - b;
+}
+
+function multiply(a, b){
+        return a * b;
+}
+
+function divide(a, b){
+        return a / b;
+}
